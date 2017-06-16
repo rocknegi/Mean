607dd31babd65b3d42850b2f3b1d195d0450d45e
rohit <rocknegi53@gmail.com>
2017-06-17 00:39:24 +0530

    mark the files for deletion slected files will become red on selection and vice versa

--- CRUD/js/controller.js
+++ CRUD/js/controller.js
@@ -1,36 +1,53 @@
-window.addEventListener("load",()=>{
-document.getElementById("add").addEventListener("click",addItem);
-clearFields();
+window.addEventListener("load", () => {
+    document.getElementById("add").addEventListener("click", addItem);
+    document.getElementById("cl").addEventListener("click", clearClass);
+    clearFields();
 });
 
-function addItem(){
-	var itemNo = document.getElementById("itemno").value;
-	var name = document.getElementById("name").value;
-	var desc = document.getElementById("desc").value;
-	var price = document.getElementById("price").value;
-	var qt = document.getElementById("qt").value;
-	var url = document.getElementById("url").value;
-	var tbody = document.getElementById("itemlist");
-	itemOperations.addNewItem(name,desc,price,qt,url);
+function clearClass() {
+    var tbody = document.getElementById("itemlist");
+    tbody.childNodes
+}
 
-	var tr = tbody.insertRow();
-	var itemObject = itemOperations.getLastObject();
-	var index = 0;
-	for(var key in itemObject){
-		tr.insertCell(index).innerHTML = itemObject[key];
-		index++;
-	}
-	tr.insertCell(index).innerHTML = "<img src='../images/delete.png'>&nbsp;<img src='../images/edit.png'>";
-	clearFields();
+function addItem() {
+    var itemNo = document.getElementById("itemno").value;
+    var name = document.getElementById("name").value;
+    var desc = document.getElementById("desc").value;
+    var price = document.getElementById("price").value;
+    var qt = document.getElementById("qt").value;
+    var url = document.getElementById("url").value;
+    var tbody = document.getElementById("itemlist");
+    itemOperations.addNewItem(name, desc, price, qt, url);
+
+    var tr = tbody.insertRow();
+    var itemObject = itemOperations.getLastObject();
+    var index = 0;
+    for (var key in itemObject) {
+        tr.insertCell(index).innerHTML = itemObject[key];
+        index++;
+    }
+
+    var deleteImg = document.createElement("img");
+    deleteImg.src = '../images/delete.png';
+    deleteImg.addEventListener("click", markForDeletion);
+    tr.insertCell(index).append(deleteImg);
+    tr.insertCell(index).innerHTML = "<img src='../images/edit.png'>";
+    clearFields();
 
 }
 
-function clearFields(){
-	var allfields = document.getElementsByClassName("clearfields");
-	for(var i = 0; i<allfields.length; i++){
-		allfields[i].value="";
-	}
-	document.getElementById("itemno").innerHTML=itemOperations.getId();
-document.getElementById("name").focus();
+function markForDeletion(event) {
+    var tr = event.srcElement.parentElement.parentElement;
+
+    tr.classList.toggle("red");
+}
+
+function clearFields() {
+    var allfields = document.getElementsByClassName("clearfields");
+    for (var i = 0; i < allfields.length; i++) {
+        allfields[i].value = "";
+    }
+    document.getElementById("itemno").innerHTML = itemOperations.getId();
+    document.getElementById("name").focus();
 
 }
