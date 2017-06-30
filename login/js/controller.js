679835ac5c0aee318b504c8cfe6cfaeedce956a4
rocknegi <rocknegi53@gmail.com>
2017-07-01 01:36:51 +0530

    add basic html layout

---
+++ login/js/controller.js
@@ -0,0 +1,5 @@
+app.controller("myctrl",($scope,myfactory)=>{
+    $scope.submit=()=>{
+        var x =location.href="pages/welcome.html";
+    }
+});
---
+++ login/js/model.js
@@ -0,0 +1,9 @@
+app.factory("myfactory",()=>{
+    var object = {
+
+
+
+    }
+    return object;
+
+    });
