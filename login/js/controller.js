e0b8de8c75eb77c2c27972c323fabceec1fcbf06
rocknegi <rocknegi53@gmail.com>
2017-07-05 12:11:34 +0530

    logic changes

--- login/js/controller.js
+++ login/js/controller.js
@@ -7,22 +7,25 @@ app.controller("myctrl",($scope,myfactory,)=>{
                   angular.forEach($scope.result, function(i,data){
                   arr.push($scope.result);
 
-                   $scope.submit=(val)=>{
-                        alert("Searching for " + val);
+                   $scope.submit=(id,pass)=>{
+                        alert("Searching for " + id);
                        console.log(arr);
                       for (var i = 0; i < arr.length; i++) {
-                     if (arr[i].one.name === val||arr[i].two.name === val||arr[i].three.name==val||arr[i].four.name==val||arr[i].five.name ==val)
+                     if (arr[i].one.name === id||arr[i].two.name === id||arr[i].three.name==id||arr[i].four.name==id||arr[i].five.name ==id)
                  {
+                     if (arr[i].one.pass === pass||arr[i].two.pass === pass||arr[i].three.pass==pass||arr[i].four.pass==pass||arr[i].five.pass ==pass)
+                         {
                      console.log("true");
 //                     $scope.store = $localStorage;
 //                     store.name = val;
-                     localStorage.setItem('session',val);
-                     console.log(localStorage.getItem('session',val));
-                     var x = location.href="pages/welcome.html"
-                     alert("Logging you in")
+                     localStorage.setItem('session',id);
+                     console.log(localStorage.getItem('session',id));
+                     var x = location.href="pages/welcome.html";
+                     alert("Logging you in");
 
                  return true;
         }
+                 }
                      else {
                          console.log("false");
                          alert("wrong id or pass");
